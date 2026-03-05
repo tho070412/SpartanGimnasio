d95067baf118c79c98ad0e1f9adb4f1cd58edd2a
Thomas Rodriguez <thomasrolo07@gmail.com>
2026-03-05 08:13:33 -0500

    Añadimos cambios en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ function Footer() {
   return (
     <footer>
       <p>© 2026 Spartan Elite Training - Todos los derechos reservados</p>
+      <p>Instagram: SpartanEliteTraining  Facebook: Spartan Elite Training</p>
     </footer>
   );
 }
